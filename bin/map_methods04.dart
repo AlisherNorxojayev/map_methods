357acc49fc2866790787d296a0123f1993eb57d1
Alisher <norxojayevalisher3@gmail.com>
2022-08-24 13:47:56 +0500

    done

--- bin/map_methods04.dart
+++ bin/map_methods04.dart
@@ -4,14 +4,14 @@ add "@" to it.
 */
 
 void main() {
-  print(func({"password": "alisher"}));
+  print(func({"password": "alisher@"}));
 }
 
 Map func(Map data) {
   if (data["password"][data["password"].length - 1] == "@") {
     return data;
   } else {
-    data["password"] = [data["password"] + "@"];
+    data["password"] = data["password"] + "@";
     return data;
   }
 }
